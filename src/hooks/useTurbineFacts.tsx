9980d07d8393029150de55b769292d2a91b4f645
hillary Ibeanu <ibeanuhillary@gmail.com>
2021-10-06 00:15:32 +0100

    validation to make sure that end date comes after start date.

--- src/hooks/useTurbineFacts.tsx
+++ src/hooks/useTurbineFacts.tsx
@@ -1,54 +1,65 @@
-import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
 import _ from "underscore";
+import { useQuery } from "react-query";
+import { useCallback, useEffect, useState } from "react";
+
 import { getAvailabilityDetails } from "../api";
 import { ITurbineDetail } from "../utils/interfaces";
 
 const useTurbineFacts = () => {
-
   const [endMonth, setEndMonth] = useState<number>(5);
   const [startMonth, setStartMonth] = useState<number>(1);
   const [availability, setAvailability] = useState<ITurbineDetail[]>();
-  const { isLoading, data, isSuccess } = useQuery<any>(['availability', { startMonth, endMonth }], getAvailabilityDetails);
 
-  console.log({ endMonth, startMonth });
+  const validateMonth = useCallback(() => {
+    const isValid = +endMonth >= +startMonth;
+    if (!isValid) console.error("End month must be after start month.");
+    return isValid;
+  }, [startMonth, endMonth]);
+
+  const { isLoading, data, isSuccess } = useQuery<any>(
+    ['availability', { startMonth, endMonth }],
+    getAvailabilityDetails, {
+    refetchOnWindowFocus: false,
+    enabled: validateMonth()
+  });
+
+  const cleanData = useCallback(() => {
+    const result: any[] = [];
+    const groupedByMonth = _.groupBy(data?.data, ({ bucket }: any) => bucket.substring(0, 7));
+
+    Object.values(groupedByMonth).forEach((groupValues) => {
+      let totalEnergyLost = 0,
+        totalEnergyProduced = 0,
+        turbineAvailability = 100,
+        worstTurbine = '';
+
+      groupValues.forEach(({ energyLost, energyProduced, turbine }) => {
+        totalEnergyLost = totalEnergyLost + energyLost;
+        totalEnergyProduced = totalEnergyProduced + energyProduced;
+        if ((energyProduced / (energyProduced + energyLost)) < turbineAvailability) {
+          worstTurbine = turbine;
+        }
+      });
+
+      const availability: any = (totalEnergyProduced / (totalEnergyProduced + totalEnergyLost)) * 100;
+      result.push({
+        worstTurbine,
+        totalEnergyLost,
+        totalEnergyProduced,
+        availability: availability.toFixed(2),
+        month: new Date(groupValues[0].bucket).toLocaleString('en-us', { month: 'long' }),
+      });
+
+    });
+    return result;
+  }, [data]);
 
   useEffect(() => {
     if (isSuccess) {
-      const groupedByMonth = _.groupBy(data?.data, ({ bucket }: any) => {
-        return bucket.substring(0, 7);
-      });
-
-      const result: any[] = [];
-
-      Object.values(groupedByMonth).forEach((groupValues) => {
-        let totalEnergyLost = 0,
-          totalEnergyProduced = 0,
-          turbineAvailability = 100,
-          worstTurbine = '';
-
-        groupValues.forEach(({ energyLost, energyProduced, turbine }) => {
-          totalEnergyLost = totalEnergyLost + energyLost;
-          totalEnergyProduced = totalEnergyProduced + energyProduced;
-          if ((energyProduced / (energyProduced + energyLost)) < turbineAvailability) {
-            worstTurbine = turbine
-          }
-        });
-
-        const availability: any = (totalEnergyProduced / (totalEnergyProduced + totalEnergyLost)) * 100;
-        result.push({
-          worstTurbine,
-          totalEnergyLost,
-          totalEnergyProduced,
-          availability: availability.toFixed(2),
-          month: new Date(groupValues[0].bucket).toLocaleString('en-us', { month: 'long' }),
-        });
-
-      });
-
+      const result = cleanData();
       setAvailability(result);
     }
-  }, [data, isSuccess]);
+  }, [data, isSuccess, cleanData]);
 
   return {
     endMonth,
