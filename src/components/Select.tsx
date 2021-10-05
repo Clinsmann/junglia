7e64f7a28313d36f018266ccde8b16bfc240ba96
hillary Ibeanu <ibeanuhillary@gmail.com>
2021-10-06 00:57:56 +0100

    updated select box styling

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -7,7 +7,7 @@ interface SelectProps {
 const Select: React.FC<SelectProps> = ({ options, onChange, value }) => (
   <select
     style={{ backgroundColor: "#363D4D" }}
-    className="text-white"
+    className="text-white px-1 py-2"
     value={value}
     onChange={onChange}>
     {options.map((value, index) => (<option key={value} value={index + 1}>{value}</option>))}
